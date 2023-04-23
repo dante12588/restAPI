2f908a48f0b5a801e1d5bfc2993f73fdb94f4cda
Przemek Ćwiertka <przemek.cwiertka@gmail.com>
2023-04-23 21:47:47 +0200

    db [user login]

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -3,6 +3,41 @@ import express from 'express';
 import { createUser, getUserByEmail } from '../db/users';
 import { random, authentications } from '../helpers';
 
+export const login = async (req: express.Request, res: express.Response) => {
+    try{
+        const { email, password } = req.body;
+
+        if( !email || !password ){
+            return res.sendStatus(404);
+        }
+
+        const user = await getUserByEmail(email).select('+authentication.salt +authentication.password');
+
+
+        if(!user){
+            return res.sendStatus(400);
+        }
+
+        const expectedHash = authentications(user.authentication.salt, password);
+
+        if(user.authentication.password !== expectedHash){
+            res.sendStatus(403);
+        }
+
+        const salt = random();
+        user.authentication.sessionToken = authentications(salt, user._id.toString());
+
+        await user.save();
+
+        res.cookie('MOJE-RESTAPI', user.authentication.sessionToken, { domain: 'localhost', path: '/' });
+
+        res.status(200).json(user).end();
+    }catch(error){
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
+
 export const register = async (req: express.Request, res: express.Response) => {
     try{
         const {email, password, username} = req.body;
